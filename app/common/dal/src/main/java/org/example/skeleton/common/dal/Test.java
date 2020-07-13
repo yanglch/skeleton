17b0734e7723d905a36e02b1cb9cb403669f3dde
ylc <529791114@qq.com>
2020-07-14 04:14:04 +0800

    init

--- app/common/dal/src/main/java/org/example/skeleton/common/dal/Test.java
+++ app/common/dal/src/main/java/org/example/skeleton/common/dal/Test.java
@@ -1,8 +1,11 @@
 package org.example.skeleton.common.dal;
 
 import com.alipay.sofa.runtime.api.annotation.SofaService;
+import org.example.skeleton.common.dal.dao.UserDODynamicSqlSupport;
 import org.example.skeleton.common.dal.dao.UserDao;
 import org.example.skeleton.common.dal.entity.UserDO;
+import org.mybatis.dynamic.sql.SortSpecification;
+import org.mybatis.dynamic.sql.SqlColumn;
 import org.mybatis.dynamic.sql.select.SelectDSLCompleter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +20,6 @@ public class Test {
     private UserDao userDao;
 //
     public List<UserDO> list() {
-        return userDao.select(SelectDSLCompleter.allRows());
+        return userDao.select(SelectDSLCompleter.allRowsOrderedBy(UserDODynamicSqlSupport.name));
     }
 }
