1496fc2fce0baa14fa6c6d46fd9620e81dc7d732
ylc <529791114@qq.com>
2020-08-02 04:30:54 +0800

    test

--- app/web/home/src/main/java/org/example/skeleton/web/home/controller/TestController.java
+++ app/web/home/src/main/java/org/example/skeleton/web/home/controller/TestController.java
@@ -9,6 +9,8 @@ import org.mybatis.dynamic.sql.insert.render.InsertStatementProvider;
 import org.mybatis.dynamic.sql.insert.render.MultiRowInsertStatementProvider;
 import org.mybatis.dynamic.sql.select.render.SelectStatementProvider;
 import org.mybatis.dynamic.sql.update.render.UpdateStatementProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,22 +29,31 @@ import java.util.stream.IntStream;
 @RequestMapping("/test")
 public class TestController {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
     @SofaReference
     private UserService userService;
 //
     @GetMapping("/1")
     public Mono<Object> helloWord()throws Exception {
+        LOGGER.info("test1");
         return Mono.delay(Duration.ofSeconds(10)).thenReturn("123fversgf34g");
     }
 
 
     @GetMapping("2")
     public Object hello() throws Exception{
+        LOGGER.info("test2");
         test();
         return "123fversgf34g";
     }
 
 
+    @GetMapping("3")
+    public Object hello3() throws Exception{
+        LOGGER.info("test3");
+        return "123fversgf34g";
+    }
 
     public Object test(){
         try {
